0b0e97c31f12e30dce0097b754bdbe5630b76255
zhouhoufa <774392980@qq.com>
2020-09-27 02:14:10 +0800

    add truffle config and remove import path instead of @uniswap

--- contracts/examples/ExampleSwapToPrice.sol
+++ contracts/examples/ExampleSwapToPrice.sol
@@ -1,8 +1,8 @@
 pragma solidity =0.6.6;
 
-import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
-import '@uniswap/lib/contracts/libraries/Babylonian.sol';
-import '@uniswap/lib/contracts/libraries/TransferHelper.sol';
+import '../../../uniswap-v2-core/contracts/interfaces/IUniswapV2Pair.sol';
+import '../../../uniswap-lib/contracts/libraries/Babylonian.sol';
+import '../../../uniswap-lib/contracts/libraries/TransferHelper.sol';
 
 import '../interfaces/IERC20.sol';
 import '../interfaces/IUniswapV2Router01.sol';
